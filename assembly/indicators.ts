ed6fd0d9ea71d0e019eb69b550df25b8073ced9e
Matthieu Moquet <matthieu@moquet.net>
2025-05-15 11:57:07 +0200

    chore: combine all indicators into a single file

--- assembly/indicators.ts
+++ assembly/indicators.ts
@@ -1,4 +1,67 @@
-import { BollingerBands } from "../types";
+import { Kline, BollingerBands } from "./types";
+
+// Highest
+export function highest(data: f64[], length: i32): f64 {
+  if (data.length === 0 || length <= 0) {
+    return 0;
+  }
+
+  return data.slice(0, length).reduce<f64>((min, val) => Math.max(val, min), data[0]);
+}
+
+// Lowest
+export function lowest(data: f64[], length: i32): f64 {
+  if (data.length === 0 || length <= 0) {
+    return 0;
+  }
+
+  return data.slice(0, length).reduce<f64>((min, val) => Math.min(val, min), data[0]);
+}
+
+// Open
+export function open(klines: Kline[], limit: i32 = klines.length): f64[] {
+  return klines.slice(0, limit).map<f64>((k) => k.open);
+}
+
+// High
+export function high(klines: Kline[], limit: i32 = klines.length): f64[] {
+  return klines.slice(0, limit).map<f64>((k) => k.high);
+}
+
+// Low
+export function low(klines: Kline[], limit: i32 = klines.length): f64[] {
+  return klines.slice(0, limit).map<f64>((k) => k.low);
+}
+
+// Close
+export function close(klines: Kline[], limit: i32 = klines.length): f64[] {
+  return klines.slice(0, limit).map<f64>((k) => k.close);
+}
+
+// Volume
+export function volume(klines: Kline[], limit: i32 = klines.length): f64[] {
+  return klines.slice(0, limit).map<f64>((k) => k.volume);
+}
+
+// HLC3 - Average of High, Low, and Close
+export function hlc3(klines: Kline[], limit: i32 = klines.length): f64[] {
+  return klines.slice(0, limit).map<f64>((k) => (k.high + k.low + k.close) / 3);
+}
+
+// HL2 - Average of High and Low
+export function hl2(klines: Kline[], limit: i32 = klines.length): f64[] {
+  return klines.slice(0, limit).map<f64>((k) => (k.high + k.low) / 2);
+}
+
+// OHLC4 - Average of Open, High, Low, and Close
+export function ohlc4(klines: Kline[], limit: i32 = klines.length): f64[] {
+  return klines.slice(0, limit).map<f64>((k) => (k.open + k.high + k.low + k.close) / 4);
+}
+
+// HLCC4 - Average of High, Low, and twice the Close
+export function hlcc4(klines: Kline[], limit: i32 = klines.length): f64[] {
+  return klines.slice(0, limit).map<f64>((k) => (k.high + k.low + k.close + k.close) / 4);
+}
 
 // SMA (Simple Moving Average)
 export function sma(source: f64[], length: i32, limit: i32 = source.length): f64[] {
--- assembly/indicators/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./klines";
-export * from "./math";
-export * from "./technical";
